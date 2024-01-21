d508c1fb442fea13659e0ef32a1bdf213b2f9220
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2024-01-21 15:23:25 -0300

    Initial Commit

---
+++ HonymoonUI/HonymoonUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonymoonUIApp.swift
+//  HonymoonUI
+//
+//  Created by Daniel Washington Ignacio on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HonymoonUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
